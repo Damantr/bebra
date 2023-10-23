0107da1860fc96780ec20554f4d56d47201502d1
Andruha <andreonepis@gmail.com>
2023-10-24 00:28:22 +0100

    Initial commit

--- src/main/java/net/andruha/bebramod/item/ModItems.java
+++ src/main/java/net/andruha/bebramod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.andruha.bebramod.item;
 
 import net.andruha.bebramod.BebraMod;
+import net.andruha.bebramod.item.custom.MetalDetectorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,6 +15,10 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> MEGA_BEBRA = ITEMS.register("mega_bebra",
             () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
+            () -> new MetalDetectorItem(new Item.Properties().durability(100)));
+    public static final RegistryObject<Item> TRUE_BREAD = ITEMS.register("true_bread",
+            () -> new Item(new Item.Properties().food(ModFoods.TRUE_BREAD)));
     public static void register (IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
